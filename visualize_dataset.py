f9f03b8b9b3c9da4d71c0df23fb4e8498c5d6205
brianlan <brianlanbo@gmail.com>
2018-05-03 14:15:46 +0800

    minor update

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -13,7 +13,7 @@ label_dir = Path('/Users/rlan/datasets/ICPR/train_1000/txt_1000')
 dataset = TianchiOCRDataset(str(im_dir), str(label_dir))
 loader = TianchiOCRDataLoader(dataset, shuffle=False)
 save_dir = Path('/tmp/icpr')
-
+im_shapes = []
 
 for label_path, (im, label) in tqdm(zip(label_dir.glob('**/*.txt'), loader)):
     rel_path = label_path.relative_to(label_dir)
@@ -21,13 +21,18 @@ for label_path, (im, label) in tqdm(zip(label_dir.glob('**/*.txt'), loader)):
     save_path.parent.mkdir(parents=True, exist_ok=True)
 
     im = im.asnumpy()
-    for pts, txt in zip(*label):
-        pts = pts.asnumpy().reshape(-1, 1, 2).astype(np.int32)
-        if txt == '###':
-            cv2.polylines(im, [pts], True, (0, 0, 0), 1)
-            cv2.line(im, (pts[0][0][0], pts[0][0][1]), (pts[2][0][0], pts[2][0][1]), (0, 0, 0))
-            cv2.line(im, (pts[1][0][0], pts[1][0][1]), (pts[3][0][0], pts[3][0][1]), (0, 0, 0))
-        else:
-            cv2.polylines(im, [pts], True, (255, 0, 0), 2)
+    # for pts, txt in zip(*label):
+    #     pts = pts.asnumpy().reshape(-1, 1, 2).astype(np.int32)
+    #     if txt == '###':
+    #         cv2.polylines(im, [pts], True, (0, 0, 0), 1)
+    #         cv2.line(im, (pts[0][0][0], pts[0][0][1]), (pts[2][0][0], pts[2][0][1]), (0, 0, 0))
+    #         cv2.line(im, (pts[1][0][0], pts[1][0][1]), (pts[3][0][0], pts[3][0][1]), (0, 0, 0))
+    #     else:
+    #         cv2.polylines(im, [pts], True, (255, 0, 0), 2)
+    #
+    # cv2.imwrite(str(save_path), cv2.cvtColor(im, cv2.COLOR_RGB2BGR))
+    im_shapes.append(im.shape)
 
-    cv2.imwrite(str(save_path), cv2.cvtColor(im, cv2.COLOR_RGB2BGR))
+im_shapes = np.array(im_shapes)
+(im_shapes[:, 0] == im_shapes[:, 1]).sum() / len(im_shapes)
+pass
